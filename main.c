edee26ac8bb015b3cf71d56995ef749d6d42967b
Raphael Mauricio Sanches de Jesus <55360548+Hargenx@users.noreply.github.com>
2023-03-15 21:32:52 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void) {
+  time_t tempo_atual;
+  struct tm *info_tempo;
+  char string_tempo[40];
+
+  // Obtem hora atual
+  time(&tempo_atual);
+  info_tempo = localtime(&tempo_atual);
+
+  // Formata Hora
+  strftime(string_tempo, sizeof(string_tempo), "%d/%m/%Y \t %H:%M:%S",
+           info_tempo);
+
+  printf("DATA \t\t HORA\n%s", string_tempo);
+  return 0;
+}
